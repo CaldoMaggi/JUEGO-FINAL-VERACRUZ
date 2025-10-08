bbdb0c6432237d4e612981b8b39d7dae5d695c78
CaldoMaggi <mquiroz984@soyudemedellin.edu.co>
2025-10-08 08:29:56 -0500

    CREACION ESCENA 3

--- LA VERACRUZ/Assets/_Scripts/CambiarEscena2.cs
+++ LA VERACRUZ/Assets/_Scripts/CambiarEscena2.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class CambiarEscena2 : MonoBehaviour
 {
- 
+    [SerializeField]
+    private GameManager gameManager;
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameManager.CargarEscena();
+        }
+    }
 }
---
+++ LA VERACRUZ/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public void CargarEscena()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
