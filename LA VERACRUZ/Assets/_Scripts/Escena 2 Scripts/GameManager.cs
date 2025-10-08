448179d143192902db4f3767314fbe16aaae7fb8
CaldoMaggi <mquiroz984@soyudemedellin.edu.co>
2025-10-08 17:13:40 -0500

    TRANSICIONES ENTRE INICIO Y ESCENA 2

--- LA VERACRUZ/Assets/_Scripts/Escena 2 Scripts/GameManager.cs
+++ LA VERACRUZ/Assets/_Scripts/Escena 2 Scripts/GameManager.cs
@@ -1,13 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]private Animator transition;
+    [SerializeField] private float transitionTime = 1f;
+
     public void CargarEscena2()
     {
         SceneManager.LoadScene(2);
     }
     public void CargarEscena()
     {
-        SceneManager.LoadScene(1);
+
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //poner la animacion
+        transition.SetTrigger("Start");
+        //esperar
+        yield return new WaitForSeconds(transitionTime);
+        //cargar la escena
+        SceneManager.LoadScene(levelIndex);
     }
 }
