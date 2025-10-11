c6438dd1835f64e468ea61a2e175096c485e2f26
Migue00523 <mtamayo494@soyudemedellin.edu.co>
2025-10-11 17:42:08 -0500

    Nivel de la iglesia

---
+++ LA VERACRUZ/Assets/_Scripts/Escena 3 Scripts/PC para Minijuegos.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PCparaMinijuegos : MonoBehaviour
+{
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+        if(isGrounded && Input.GetKeyDown(KeyCode.UpArrow))
+        {   
+            rb.AddForce(Vector2.up* jumpForce, ForceMode2D.Impulse);
+            Debug.log("Saltando");
+        }
+
+
+void Start()
+    {
+        // 🔹 Salto con flecha arriba
+        Input.GetKeyDown(KeyCode.UpArrow);
+       
+
+        
+
+    // 🔹 Detectar si está tocando el suelo
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Piso"))
+            isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Piso"))
+            isGrounded = false;
+    }
+}
