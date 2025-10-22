2ebc6872beb9db76db64673f74420e49485f80c3
Migue00523 <mtamayo494@soyudemedellin.edu.co>
2025-10-21 23:01:58 -0500

    DIseño y Arreglo del nivel del Palacio

--- LA VERACRUZ/Assets/_Scripts/Escena 3 Scripts/TrenManager.cs
+++ LA VERACRUZ/Assets/_Scripts/Escena 3 Scripts/TrenManager.cs
@@ -5,25 +5,20 @@ public class TrenManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject tren;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
     void Start()
     {
         StartCoroutine(TrenLoop());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     IEnumerator TrenLoop()
     {
         tren.SetActive(true);
 
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(5);
         tren.SetActive(false);
-        tren.transform.position = new Vector3(-40f , -5f, 0);
+        tren.transform.position = new Vector3(-35.7f , -6.8f, 0);
         yield return new WaitForSeconds(1);
 
        StartCoroutine(TrenLoop());
